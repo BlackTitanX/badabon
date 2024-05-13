d68a1b9b302c0fe76ad1239fceacb05c32773b09
Israel <israelperezmasle2@gmail.com>
2024-05-13 19:14:24 -0400

    fixed responsiveness on top bar

--- footer.php
+++ footer.php
@@ -106,4 +106,7 @@ Este sitio web utiliza cookies para asegurarse de obtener la mejor experiencia e
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
--- navbar.php
+++ navbar.php
@@ -1,68 +1,49 @@
 <?php
 require_once "manager.php";
 ?>
-<!DOCTYPE html>
-
-<div class="section" id="b-section-header" name="Header"><div class="widget Header" data-version="2" id="Header1">
-<div class="header image-placement-behind no-image">
-  <h1 id="banner-letters">SOMOS NOTICIAS </h1>
-<div class="container" style="background-image: url('https://asset.cloudinary.com/dbllecqwm/b4f9d26d38ba5a1ceae540fd984c441a');">
-
-</div>
-</div>
-</div>
-</div>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-
-      
-      <div class="container">
-    <a class="navbar-brand" href="index.php"><span><img src="https://res.cloudinary.com/dbllecqwm/image/upload/v1715528143/pytx5uinfzpoju1cgkay.png" class="custom-logo" alt=""></span>AMADO CLICK</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="index.php">INICIO</a>
-      </div>
-    </div>
-    <form class="form-inline my-2 my-lg-0">
-      <?php
-      if(isset($_SESSION["email"]))
-      {
-        if($authority == "Admin")
-        {
-          ?>
-           <div class="nav-item">
-            <div class="">
-              <a class="" href="blog/addblog.php">Add Text</a>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SOMOS NOTICIAS</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<div class="section" id="b-section-header" name="Header">
+    <div class="widget Header" data-version="2" id="Header1">
+        <div class="header image-placement-behind no-image">
+            <h1 id="banner-letters" class="text-center">SOMOS NOTICIAS</h1>
+            <div class="container" style="background-image: url('https://asset.cloudinary.com/dbllecqwm/b4f9d26d38ba5a1ceae540fd984c441a'); background-size: cover;">
             </div>
         </div>
-          <?php
-        }
-        ?>
-        
-        <a class="nav-link" href="./profile.php">Perfil</a>
-        <a class="nav-link" href="./logout.php">Salir</a>
-        <?php
-      }
-      ?>
-    </form>
-  </div>
-
-<div class="ml-auto my-2 my-lg-0 mt-2">
-<div class="section" id="b-section-navbar-search-form" name="Navbar: search form">
-<div class="widget BlogSearch mt-2" data-version="2" id="BlogSearch1">
-<form action="" class="form-inline">
-<input aria-label="Search this blog" class="form-control mr-sm-2" name="q" placeholder="Search this blog" type="text">
-</form>
-</div></div>
+    </div>
 </div>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">
+            <img src="https://res.cloudinary.com/dbllecqwm/image/upload/v1715528143/pytx5uinfzpoju1cgkay.png" class="custom-logo" alt=""> AMADO CLICK
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="index.php">INICIO</a>
+                <?php if(isset($_SESSION["email"])): ?>
+                    <?php if($authority == "Admin"): ?>
+                        <a class="nav-link" href="blog/addblog.php">Add Text</a>
+                    <?php endif; ?>
+                    <a class="nav-link" href="./profile.php">Perfil</a>
+                    <a class="nav-link" href="./logout.php">Salir</a>
+                <?php endif; ?>
+            </div>
+            <form class="form-inline ml-auto">
+                <input aria-label="Search this blog" class="form-control mr-sm-2" name="q" placeholder="Search this blog" type="text">
+            </form>
         </div>
-      </div>
-    </nav>
-    
+    </div>
+</nav>
 
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    
-    
