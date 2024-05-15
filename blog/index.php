dd51d7ba55a1da4087196c8efb099dd042f784b3
Israel <israelperezmasle2@gmail.com>
2024-05-15 08:17:13 -0400

    removed visit counter

--- blog/index.php
+++ blog/index.php
@@ -23,9 +23,7 @@ require_once "../manager.php";
 <?php include "../navbar.php"?>
 
 	
-<!-- Contador de visitas -->
-<div id="sfclc1w6xfhuszm7rtdexkcure2dm8313qg"></div><script type="text/javascript" src="https://counter4.optistats.ovh/private/counter.js?c=lc1w6xfhuszm7rtdexkcure2dm8313qg&down=async" async></script><noscript><a href="https://www.contadorvisitasgratis.com" title="contador de visitas"><img src="https://counter4.optistats.ovh/private/contadorvisitasgratis.php?c=lc1w6xfhuszm7rtdexkcure2dm8313qg" border="0" title="contador de visitas" alt="contador de visitas"></a></noscript>
-<!-- Fin Contador de visitas -->
+
 
 
     <!-- Page Content -->
